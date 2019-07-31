b4350bb57c54214a84c7e255366eb9b28302cb53
oknaveen <oknaveen9395@gmail.com>
2019-07-31 11:48:40 -0400

    Implemented the service class with  business logics

--- ExpenseTrackerAPI/src/main/java/com/naveen/api/service/ExpenseService.java
+++ ExpenseTrackerAPI/src/main/java/com/naveen/api/service/ExpenseService.java
@@ -1,5 +1,20 @@
 package com.naveen.api.service;
 
+import java.util.List;
+
+import com.naveen.api.domain.Expense;
+
 public interface ExpenseService {
 
+	List<Expense> findAll();
+
+	List<Expense> findByMonthAndYear(String month, int year);
+
+	List<Expense> findByYear(Integer year);
+
+	void saveOrUpdateExpense(Expense expense);
+
+	void deleteExpense(String id);
+	
+	
 }
--- ExpenseTrackerAPI/src/main/java/com/naveen/api/service/ExpenseServiceImpl.java
+++ ExpenseTrackerAPI/src/main/java/com/naveen/api/service/ExpenseServiceImpl.java
@@ -1,5 +1,45 @@
 package com.naveen.api.service;
 
-public class ExpenseServiceImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.naveen.api.domain.Expense;
+import com.naveen.api.repositories.ExpenseRepository;
+
+@Service
+public class ExpenseServiceImpl implements ExpenseService {
+	
+	@Autowired
+	ExpenseRepository expenseRepository;
+
+	@Override
+	public List<Expense> findAll() {
+		return (List<Expense>) expenseRepository.findAll();
+	}
+
+	@Override
+	public List<Expense> findByMonthAndYear(String month, int year) {
+		return expenseRepository.findByMonthAndYear(month, year);
+	}
+
+	@Override
+	public List<Expense> findByYear(Integer year) {
+		return expenseRepository.findByYear(year);
+	}
+
+	@Override
+	public void saveOrUpdateExpense(Expense expense) {
+		expenseRepository.save(expense);
+		
+	}
+
+	@Override
+	public void deleteExpense(String id) {
+		expenseRepository.deleteById(id);
+		
+		
+	}
 
 }
