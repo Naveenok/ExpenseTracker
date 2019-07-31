64749cbd2a6f5d4c14f2c561de27a5fd06ef76e2
oknaveen <oknaveen9395@gmail.com>
2019-07-31 01:40:40 -0400

    created the Entity class

--- ExpenseTrackerAPI/src/main/java/com/naveen/api/domain/Expense.java
+++ ExpenseTrackerAPI/src/main/java/com/naveen/api/domain/Expense.java
@@ -1,5 +1,49 @@
 package com.naveen.api.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Expense {
+	@Id
+	private String id;
+	private String description;
+	private Integer amount;
+	private String month;
+	private Integer year;
+	
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Integer getAmount() {
+		return amount;
+	}
+	public void setAmount(Integer amount) {
+		this.amount = amount;
+	}
+	public String getMonth() {
+		return month;
+	}
+	public void setMonth(String month) {
+		this.month = month;
+	}
+	public Integer getYear() {
+		return year;
+	}
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+	
+	
 
 }
