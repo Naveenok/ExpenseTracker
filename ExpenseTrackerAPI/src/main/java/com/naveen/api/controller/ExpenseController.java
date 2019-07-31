2ee8530a9571b4160ffac5884ea75e314d2ad76b
oknaveen <oknaveen9395@gmail.com>
2019-07-31 17:30:41 -0400

    added the properties for the database connection and the controller methods have been defined

--- ExpenseTrackerAPI/src/main/java/com/naveen/api/controller/ExpenseController.java
+++ ExpenseTrackerAPI/src/main/java/com/naveen/api/controller/ExpenseController.java
@@ -1,5 +1,58 @@
 package com.naveen.api.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.naveen.api.domain.Expense;
+import com.naveen.api.service.ExpenseService;
+
+@RestController
+@RequestMapping("/expense")
 public class ExpenseController {
+
+	@Autowired
+	ExpenseService expenseService;
+
+	@GetMapping
+	public ResponseEntity<?> getAll() {
+		List<Expense> result = expenseService.findAll();
+		return new ResponseEntity(result, HttpStatus.OK);
+
+	}
+
+	@GetMapping("/{year}/{month}")
+	public ResponseEntity<?> getByMonthYear(@PathVariable("year") Integer year, @PathVariable("month") String month) {
+		List<Expense> result = new ArrayList<>();
+		if ("All".equals(month)) {
+			result = expenseService.findByYear(year);
+
+		} else {
+			result = expenseService.findByMonthAndYear(month, year);
+		}
+		return new ResponseEntity(result, HttpStatus.OK);
+	}
+
+	@PostMapping
+	public ResponseEntity<?> addorUpdateExpense(@RequestBody Expense expense) {
+		expenseService.saveOrUpdateExpense(expense);
+		return new ResponseEntity("Expense added successfully", HttpStatus.OK);
+	}
+
+	@DeleteMapping
+	public void deleteExpense(@RequestParam("id") String id) {
+		expenseService.deleteExpense(id);
+	}
 
 }
--- ExpenseTrackerAPI/src/main/java/com/naveen/api/service/ExpenseService.java
+++ ExpenseTrackerAPI/src/main/java/com/naveen/api/service/ExpenseService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.naveen.api.domain.Expense;
 
+
 public interface ExpenseService {
 
 	List<Expense> findAll();
