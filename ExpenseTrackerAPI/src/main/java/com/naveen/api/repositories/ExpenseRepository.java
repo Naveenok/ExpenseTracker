90e9cd05b4f5dd766cf03e65c2e023da25e43f27
oknaveen <oknaveen9395@gmail.com>
2019-07-31 11:14:56 -0400

    Interface created to perform the CRUD operations using the defined methods

--- ExpenseTrackerAPI/src/main/java/com/naveen/api/repositories/ExpenseRepository.java
+++ ExpenseTrackerAPI/src/main/java/com/naveen/api/repositories/ExpenseRepository.java
@@ -1,5 +1,16 @@
 package com.naveen.api.repositories;
 
-public interface ExpenseRepository {
+import java.util.List;
 
+import org.springframework.data.repository.CrudRepository;
+
+import com.naveen.api.domain.Expense;
+//perform CRUD operations
+public interface ExpenseRepository extends CrudRepository<Expense, String> {//Takes domain class and the id as the parameter
+	//Implementation will be done by springboot automatically
+	List<Expense> findByMonthAndYear(String month, int year);
+
+	List<Expense> findByYear(Integer year);
+	
+	
 }
